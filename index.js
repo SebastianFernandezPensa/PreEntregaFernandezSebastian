9dfaf3818e6d6c9d4c93cede14b29751e702b9ac
Sebastian Fernandez <sebastianfernandez772@gmail.com>
2022-12-13 20:40:01 -0300

    segundaPreEntrega

--- index.js
+++ index.js
@@ -1,69 +1,61 @@
+// BASE PARA ECOMERCE
 
-// Base de datos para registrar usuarios
-
-let nick = prompt ('Ingrese su usuario')
-
-let newPassword = prompt ('Ingrese su password')
-
-let ingreseSuMail = prompt ('Ingrese su email')
-
-let siTiene = false
-
-let siContiene = false
-
-let siTieneOcho = false
-
-
-function arroba (){
-while (siContiene=true){
-
-    if ( ingreseSuMail.match(/[@]/)){
-        siContiene=true
-        break;
+class alimentos {
+    constructor (id,nombre,vencimiento,precio,oferta){
+        this.id=id
+        this.nombre=nombre
+        this.vencimiento=vencimiento
+        this.precio=precio
+        this.oferta=oferta
     }
-    else {
-    alert ('Su email debe tener arroba')
-    ingreseSuMail = prompt ('Ingrese su email')
-        }
+    ofertaAcumulada(){
+        if (this.oferta === true){
+            return 'Usted tiene un descuento del 10% en este producto'
+        } else { 
+            return 'No tiene descuento en este producto'
+            }
     }
 }
+    
+
+const producto = []
 
 
-function number (){
-while (siTiene=true){
+producto.push(
+new alimentos (2,'leche','20/12/2023','$200',true),
+new alimentos (5,'arroz','20/05/2040','$150',false),
+new alimentos (4,'maniKing','20/05/2040','$200',false),
+new alimentos (3,'arbejas','20/05/2040','$210',false),
+new alimentos (1,'dulceDeLeche','20/05/2040','$300',false),
+new alimentos (8,'mantecol','20/05/2040','$350',true),
+new alimentos (7,'pan','20/05/2040','$100',false),
+new alimentos (6,'alfajorPepito','20/05/2040','$150',true),
+new alimentos (0,'azucar','20/05/2040','$120',false)
+)
 
-    if ( newPassword.match(/[123456789]/)){
-        siTiene=true
-        break;
-    }
-    else {
-    alert ('Su password debe tener caracteres alfanumericos')
-    newPassword = prompt ('Ingrese su password')
-    }
-    }
-}
-
-function passwordCaracteres (){
-while (siTieneOcho=true){
-    if ( newPassword.length < password.length){
-        alert ('Pasword ingresada debe tener mas de ocho caracteres')
-        newPassword = prompt ('Ingrese su password')
-
+producto.sort((a,b)=>{
+    if(a.id < b.id){
+        return -1
+    } else if (a.id > b.id){
+        return 1
     } else {
-        siTiene = true;
-        alert (`Bienvenido usuario ${nick}, su email : ${ingreseSuMail} y password fueron registrados correctamente, gracias!`)
-        break;
+        return 0
     }
+})
+
+console.table(producto)
+
+function filtro (){
+    const filtrado = producto.filter(elemento => elemento.oferta === true)
+    let ofert = confirm ('Quieres ver las ofertas?')
+    if ( ofert === true){
+        console.table(filtrado)
+        console.log('Estos productos tiene 10% de descuento')
+    }
+    
 }
-}
 
-
-number()
-
-arroba()
-
-passwordCaracteres()
-
+filtro()
 
 
 
